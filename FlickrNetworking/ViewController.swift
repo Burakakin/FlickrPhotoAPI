84aa682a8c3202d9ed2c9ea6ea994330986a908a
Burak Akın <akinburak5@gmail.com>
2019-08-12 22:15:09 +0300

    Initial Commit

---
+++ FlickrNetworking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrNetworking
+//
+//  Created by Burak Akin on 12.08.2019.
+//  Copyright © 2019 Burak Akin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
